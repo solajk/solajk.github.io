23244084bd54b4917d9b8c09fa5d80a4ce40dcf6
solajk <jkusenda@gmail.com>
2021-02-15 00:32:41 +0000

    saving 021421

--- interest-cal/script.js
+++ interest-cal/script.js
@@ -1,14 +1,21 @@
-function messageTemplate(principal, rate, year, finalYear) {
-    return `xxx 
-    <span class="yellow">${principal}hi</span>`
+function messageTemplate(principal, rate, interest, finalYear) {
+    return `If you deposit <span class="yellow">${principal}</span>,<br/>
+    at an interest rate of <span class="yellow">${rate}%</span>.<br/>
+    You will receive an amount of <span class="yellow">${interest}</span>,<br/>
+    in the year <span class="yellow">${finalYear}</span>`
 }
-
-
 function compute() {
     //get values from user input
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
+    var years = parseInt(document.getElementById("years").value);
+    //validate principal input
+    if (principal <= 0){
+        alert("Enter a positive number")
+        document.getElementById("result").innerHTML = ""
+        document.getElementById("principal").focus()
+        return
+    }
     //calculate interest with formula
     var interest = principal * years * rate / 100;
     //convert years
@@ -16,11 +23,7 @@ function compute() {
     var curYear = today.getFullYear();
     var finalYear = curYear + years;
     //display message with amounts
-    var message = 'If you deposit ' + principal + ",";
-    console.log(interest)
-    document.getElementById("result").innerHTML = messageTemplate(principal, rate, curYear, finalYear);
-
-
+    document.getElementById("result").innerHTML = messageTemplate(principal, rate, interest, finalYear);
 }
 function displayRate() {
     var rate = document.getElementById("rate").value;
